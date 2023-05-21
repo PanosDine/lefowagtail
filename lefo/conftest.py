fda833536a59cd0b9d8523060d859a08b05550d2
Panose <panagiotisntine@yahoo.com>
2023-05-21 18:10:20 +0100

    first commit

---
+++ lefo/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from lefo.users.models import User
+from lefo.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
